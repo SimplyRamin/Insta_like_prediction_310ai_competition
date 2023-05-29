c1ef748204721b6463fc5dae23277d22d9a14561
Ramin Ferdos <ferdos.ramin@gmail.com>
2023-05-29 15:30:59 +0330

    object detection functionality done and working, now the result of the page information and image object must be pased to the xgboost model

--- Server.py
+++ Server.py
@@ -10,6 +10,7 @@ import cv2
 from InstagramBot import InstagramBot
 from flask import Flask, request, Response
 from flask_restful import Resource, Api, reqparse
+import torch
 from torchvision import models, transforms
 import base64
 
@@ -23,6 +24,8 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[.485, .456, .406],
                          std=[.229, .224, .225])
 ])
+with open('Data/ilsvrc2012_wordnet_lemmas.txt', 'r') as f:
+    categories = [s.strip() for s in f.readlines()]
 app = Flask(__name__)
 api = Api(app)
 
@@ -37,10 +40,6 @@ api = Api(app)
 
 
 class Predict(Resource):
-    ...
-
-
-class Test(Resource):
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('image', required=True)
@@ -52,13 +51,22 @@ class Test(Resource):
         nparr = np.frombuffer(image_encoded, np.uint8)
         image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        input_tensor = transform(image_rgb)
+        input_batch = input_tensor.unsqueeze(0)
+        with torch.no_grad():
+            output = efficient_net(input_batch)
+        detected_object = torch.nn.functional.softmax(output[0], dim=0)
+        prob, cat = torch.topk(detected_object, 1)
+
         return {
-            'message': f'Image Recieved, size: {image.shape[1], image.shape[0]}'
+            'message': f'Image Recieved, size: {image.shape[1], image.shape[0]}',
+            'object': f'{categories[cat[0]]}',
+            'prob': f'{prob[0]:.4f}',
         }, 200
 
 
 api.add_resource(Predict, '/predict')
-api.add_resource(Test, '/test')
 
 if __name__ == '__main__':
     app.run()
