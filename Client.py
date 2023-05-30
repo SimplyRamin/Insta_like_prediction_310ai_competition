526f73889e393e808c4294f3dd4417c15c8c29bf
Ramin Ferdos <ferdos.ramin@gmail.com>
2023-05-30 11:43:31 +0330

    changes to client for getting post type from user, and preparing some parts of prediction with xgboost. there is a problem in projecting the features list as prediction to the xgboost model. have to find a way to convert the input list of a model to the working one.

--- Client.py
+++ Client.py
@@ -24,7 +24,7 @@ headers = {'content-type': content_type}
 console.print('Welcome to the Instagram like predictor client, Designed by [header]Ramin F. for @310.ai Competition[/].')
 console.print('Please copy the image you want to process into the Input folder and type the name of it plus its format:')
 console.print('for example: image1.jpg')
-image_name = input('>')
+image_name = input('> ')
 if path.isfile(f'{input_address}{image_name}'):
     image = cv2.imread(f'{input_address}{image_name}')
     image_format = '.' + image_name.split('.')[1]
@@ -35,12 +35,21 @@ else:
     console.print('Rerun the program.', style='progress')
     exit()
 
+console.print('Please specify whether you want to share this image solely or in a carousel?')
+console.print('Choose from: single / carousel')
+post_type = input('> ')
+if post_type != 'single' or post_type != 'carousel':
+    console.print('Please either enter "single" or "carousel".', style='error')
+    console.print('Rerun the program.', style='progress')
+    exit()
+
 console.print('Please enter the name of profile that this image will be published in.')
 console.print('for example: natgeo')
-page = input('>')
+page = input('> ')
 image_string = base64.b64encode(image_encoded.tobytes())
 image_string = str(image_string.decode())
 response = requests.post(api_address, json={'image': image_string,
-                                            'page': str(page)}, headers=headers)
+                                            'page': str(page),
+                                            'post_type': str(post_type)}, headers=headers)
 
 console.print(json.loads(response.text))
